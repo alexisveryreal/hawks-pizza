0f7879587c9b43a29e9c2edd4dc7de4e1abc833d
alexisveryreal <alexmaple20@gmail.com>
2022-01-29 19:16:03 -0800

    * start of expo project with typescript * setup fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
+import AppLoading from "expo-app-loading";
+
+import colors from "./assets/colors/colors";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textStyle}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Ionicons name="md-checkmark-outline" size={32} color={colors.primary} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontFamily: "Montserrat_500Medium",
+    color: colors.secondary,
   },
 });
